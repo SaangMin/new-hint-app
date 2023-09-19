e93dbaf7bf8563638d8e869ebe2375d52aa49601
skysmyoo7 <skysmyoo7@gmail.com>
2023-09-19 16:44:16 +0900

    viewModel 설계

--- app/src/main/java/com/skysmyoo/new_hint_app/ui/StoreViewModel.kt
+++ app/src/main/java/com/skysmyoo/new_hint_app/ui/StoreViewModel.kt
@@ -1,11 +1,13 @@
 package com.skysmyoo.new_hint_app.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.skysmyoo.new_hint_app.data.model.StoreModel
 import com.skysmyoo.new_hint_app.data.source.repository.StoreRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,6 +17,21 @@ class StoreViewModel @Inject constructor(
     private val _storeModel = MutableStateFlow<StoreModel?>(null)
     val storeModel: StateFlow<StoreModel?> = _storeModel
 
-    
+    fun findStore(code: String) {
+        viewModelScope.launch {
+            val storeModel = repository.findStore(code)
+            _storeModel.value = storeModel
+        }
+    }
 
+    fun getStore() {
+        viewModelScope.launch {
+            val storeModel = repository.getStore()
+            _storeModel.value = storeModel
+        }
+    }
+
+    fun getStoreCode(): String? {
+        return repository.getStoreCode()
+    }
 }
