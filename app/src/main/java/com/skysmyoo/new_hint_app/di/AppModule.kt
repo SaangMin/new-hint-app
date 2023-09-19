8d80d2c9f1e8b9ff5648fcb5acb6e694282831cd
skysmyoo7 <skysmyoo7@gmail.com>
2023-09-19 16:43:18 +0900

    di 구축

--- app/src/main/java/com/skysmyoo/new_hint_app/di/AppModule.kt
+++ app/src/main/java/com/skysmyoo/new_hint_app/di/AppModule.kt
@@ -1,13 +1,42 @@
 package com.skysmyoo.new_hint_app.di
 
+import android.content.Context
+import androidx.room.Room
+import com.skysmyoo.new_hint_app.data.source.local.AppDatabase
+import com.skysmyoo.new_hint_app.data.source.local.SharedPreferencesManager
+import com.skysmyoo.new_hint_app.data.source.local.StoreModelDao
 import com.squareup.moshi.Moshi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
 
     val moshi: Moshi by lazy { Moshi.Builder().build() }
+
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "hint-database"
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    fun provideStoreModelDao(appDatabase: AppDatabase): StoreModelDao {
+        return appDatabase.storeModelDao()
+    }
+
+    @Provides
+    @Singleton
+    fun providePreferencesManager(@ApplicationContext context: Context): SharedPreferencesManager {
+        return SharedPreferencesManager(context)
+    }
 }
