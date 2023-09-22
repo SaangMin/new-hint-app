3f8c3b51f79299affc0c98ec78b387ea20708326
skysmyoo7 <skysmyoo7@gmail.com>
2023-09-22 11:11:38 +0900

    시간 설정 로직 구현

---
+++ app/src/main/java/com/skysmyoo/new_hint_app/utils/TimeFormat.kt
@@ -0,0 +1,9 @@
+package com.skysmyoo.new_hint_app.utils
+
+object TimeFormat {
+    fun formatTime(seconds: Int): String {
+        val minutes = seconds / 60
+        val remainingSeconds = seconds % 60
+        return String.format("%02d:%02d", minutes, remainingSeconds)
+    }
+}
