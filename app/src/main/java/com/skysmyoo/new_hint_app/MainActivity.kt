0015679372cd0cd453a5581a9529e2d43bee319b
skysmyoo7 <skysmyoo7@gmail.com>
2023-09-22 11:13:22 +0900

    HintScreen 구현

--- app/src/main/java/com/skysmyoo/new_hint_app/MainActivity.kt
+++ app/src/main/java/com/skysmyoo/new_hint_app/MainActivity.kt
@@ -8,26 +8,31 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.skysmyoo.new_hint_app.ui.home.HomeScreen
 import com.skysmyoo.new_hint_app.ui.LoginScreen
 import com.skysmyoo.new_hint_app.ui.StoreViewModel
+import com.skysmyoo.new_hint_app.ui.hint.HintScreen
+import com.skysmyoo.new_hint_app.ui.hint.HintViewModel
+import com.skysmyoo.new_hint_app.ui.home.HomeScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     private val storeViewModel: StoreViewModel by viewModels()
+    private val hintViewModel: HintViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigation(storeViewModel)
+            Navigation(storeViewModel, hintViewModel)
         }
     }
 }
 
 @Composable
 fun Navigation(
-    storeViewModel: StoreViewModel
+    storeViewModel: StoreViewModel,
+    hintViewModel: HintViewModel,
 ) {
     val navController = rememberNavController()
 
@@ -41,6 +46,14 @@ fun Navigation(
 
         composable("home") {
             HomeScreen(navController, storeViewModel)
+        }
+
+        composable("hintScreen/{themeId}") { backStackEntry ->
+            HintScreen(
+                navController = navController,
+                viewModel = hintViewModel,
+                themeId = backStackEntry.arguments?.getString("themeId") ?: ""
+            )
         }
     }
 }
