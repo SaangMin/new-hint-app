e44ea4a9041880518818ac05e2f27f5159d51ec2
skysmyoo7 <skysmyoo7@gmail.com>
2023-09-26 09:51:39 +0900

    지점 비밀번호 입력창 구성

--- app/src/main/java/com/skysmyoo/new_hint_app/utils/Admin.kt
+++ app/src/main/java/com/skysmyoo/new_hint_app/utils/Admin.kt
@@ -1,10 +1,17 @@
 package com.skysmyoo.new_hint_app.utils
 
+import com.skysmyoo.new_hint_app.HintApplication
+
 object Admin {
 
     private const val masterPassword = "0945"
+    private val preferencesManager = HintApplication.preferencesManager
 
     fun isCorrectMasterPassword(password: String): Boolean {
         return password == masterPassword
+    }
+
+    fun isCorrectStorePassword(password: String): Boolean {
+        return password == preferencesManager.getPassword()
     }
 }
